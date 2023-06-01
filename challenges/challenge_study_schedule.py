e28ada58939faa076487989215cda41299fc8121
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-01 17:59:29 -0300

    feat: solves challenge 1 - study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    time_frequency = 0
+    for period in permanence_period:
+        (start_time, end_time) = period
+        if (
+            type(start_time) is not int
+            or type(end_time) is not int
+            or type(target_time) is not int
+        ):
+            return None
+        if start_time <= target_time <= end_time:
+            time_frequency += 1
+
+    return time_frequency
