2d3ff6ddf266e0ed367414d9aee08e95bccaf7e1
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-01 20:17:09 -0300

    feat: solves challenge 3 - is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    if high_index < low_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        if len(word) == 2:
+            return True
+        else:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    else:
+        return False
