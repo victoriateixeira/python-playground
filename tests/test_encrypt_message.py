6bd150e282f4e358bf76e46295f7b73a4b022ea9
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-01 19:36:58 -0300

    feat: solves challenge 2 - encrypt_message_tests

---
+++ tests/test_encrypt_message.py
@@ -0,0 +1,24 @@
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+
+def test_message_is_string():
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(234567, 2)
+
+
+def test_key_is_number():
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("abcdef", "2")
+
+
+def test_key_higher_than_message_length():
+    assert encrypt_message("abcdef", 7) == "fedcba"
+
+
+def test_key_is_odd():
+    assert encrypt_message("abcdef", 3) == "cba_fed"
+
+
+def test_key_is_even():
+    assert encrypt_message("abcdef", 2) == "fedc_ba"
