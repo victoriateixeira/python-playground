fad5c1c5c3d74747f36f5d88b2b80957ccd9f4f8
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-05 20:19:10 -0300

    feat: solves challenge 5 - find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,33 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) <= 1:
+        return False
+    sorted_nums = sorted(nums)
+    for idx, num in enumerate(sorted_nums):
+        if type(num) is not int or num < 1:
+            return False
+
+        result = binary_search(sorted_nums, num, idx)
+        if result != -1:
+            return result
+    return False
+
+
+def binary_search(numbers, target, idx):
+    start = 0
+    end = len(numbers) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if numbers[mid] == target and mid != idx:
+            return target
+
+        if target < numbers[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return -1
+
+
+print(find_duplicate([1, 3, 4, 2, 2]))
