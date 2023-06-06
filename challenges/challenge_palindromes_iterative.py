130ce811b8eca4bcbf1100baafbd03df3380ced9
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-05 22:33:46 -0300

    feat: solves challenge 6 - palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    result = False
+
+    for i in range((len(word) + 1) // 2):
+        if word[i] == word[len(word) - 1 - i] or len(word) == 2:
+            result = True
+        else:
+            result = False
+            break
+
+    return result
